1e5747db296c82894aa16e7aaf8c24f48cedf52d
Jacob Shaw <jacob.shaw@unb.ca>
2018-11-15 20:02:13 -0400

    Init

---
+++ CI_Demo.py
@@ -0,0 +1,8 @@
+def func1(x):
+	return x+x+x
+	
+def func2(x):
+	return x*2
+
+def func3(x):
+	return x/x
---
+++ tests.py
@@ -0,0 +1,8 @@
+from CI_Demo import *
+
+def test_func1():
+	assert func1(3) == 9
+def test_func2():
+	assert func2(3) == 6
+def test_func3():
+	assert func3(3) == 1
